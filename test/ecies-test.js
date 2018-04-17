0f4bb5f8ac7f1664fc289c0ea1dc16c83300a7fe
Doresimon <doresimon@163.com>
2018-04-17 16:58:12 +0800

    remove remark...

--- test/ecies-test.js
+++ test/ecies-test.js
@@ -32,31 +32,3 @@ describe('ecies', function() {
         ]);
     });
 });
-
-
-
-
-
-
-//
-//
-//
-//
-//
-//
-// let priv = '0a6aed3f0099eab4aec8ca45d06684e7228e98c83044321eb81d8b4919bc2f8f';
-// let privB =  '0fc9775c67772cbb89dbd3d7a7b0db1ce51f46cdd361b0d4308b953df0da9380';
-// let pubB =  '6b42d8eb0c1f0581652a3e6fd6c6dee5b5a7114a5640684e59658f25bcb130ca';
-// let msg =  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-// let iv = "1234567890123456"; //32Byte   ???
-//
-// ecies.setKeyPair(priv);
-// let cxt = ecies.enc(pubB, msg, iv);
-//
-// ecies.setKeyPair(privB);
-// let plain = ecies.dec(cxt.msg_cxt, cxt.out,  cxt.iv);
-//
-//
-// console.log(msg)
-// console.log(cxt)
-// console.log(plain)
